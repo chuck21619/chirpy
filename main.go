1eaaa4d28e19ca92b17ae9e495b5b07cf2c0a347
chuck21619 <charles.ward.johnston@gmail.com>
2024-09-13 21:12:09 -0400

    add fileserver

--- main.go
+++ main.go
@@ -6,11 +6,29 @@ import (
 
 func main() {
 
-	mux := http.NewServeMux()
-	srv := http.Server{
+	multiplexer := http.NewServeMux()
+	server := http.Server{
 		Addr:    ":8080",
-		Handler: mux,
+		Handler: multiplexer,
 	}
 	
-	srv.ListenAndServe()
+	
+	filesDirectory := http.Dir(".")
+	fileServerHandler := http.FileServer(filesDirectory)
+	strippedPrefixFileServerHandler := http.StripPrefix("/app", fileServerHandler)
+	multiplexer.Handle("/app/", strippedPrefixFileServerHandler)
+	//multiplexer.Handle("/", fileServerHandler)
+
+	multiplexer.HandleFunc("/healthz", mysWEetnest)
+
+	server.ListenAndServe()
+}
+
+func mysWEetnest(responseWriter http.ResponseWriter, request *http.Request) {
+	responseWriter.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	//responseWriter.WriteHeader(http.StatusOK) //unnecessary
+	statusString := http.StatusText(http.StatusOK)
+	//statusString := "any string i fucking want bitch"
+	responseWriter.Write([]byte(statusString))
+	
 }
