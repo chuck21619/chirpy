9df8c4932bddedc282c6e01e63b6edee03227725
chuck21619 <charles.ward.johnston@gmail.com>
2024-09-17 09:13:32 -0400

    add some bullshit

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"fmt"
 )
 
 func main() {
@@ -12,14 +13,21 @@ func main() {
 		Handler: multiplexer,
 	}
 	
+	apiConfig := apiConfig{
+		fileserverHits: 0,
+	}
 	
 	filesDirectory := http.Dir(".")
 	fileServerHandler := http.FileServer(filesDirectory)
 	strippedPrefixFileServerHandler := http.StripPrefix("/app", fileServerHandler)
-	multiplexer.Handle("/app/", strippedPrefixFileServerHandler)
+	wrappedStrippedFileServerHandler := apiConfig.middlewareMetricsInc(strippedPrefixFileServerHandler)
+	multiplexer.Handle("/app/", wrappedStrippedFileServerHandler)
 	//multiplexer.Handle("/", fileServerHandler)
 
 	multiplexer.HandleFunc("/healthz", mysWEetnest)
+
+	multiplexer.HandleFunc("/metrics", apiConfig.handlerMetrics)
+	multiplexer.HandleFunc("/reset", apiConfig.handlerReset)
 
 	server.ListenAndServe()
 }
@@ -32,3 +40,27 @@ func mysWEetnest(responseWriter http.ResponseWriter, request *http.Request) {
 	responseWriter.Write([]byte(statusString))
 	
 }
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
+}
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
+
